5e4ad9646d3790ee67e2297361c9a47af193c3fb
SISITheAI <sisibernoulli@gmail.com>
2025-04-14 11:02:27 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Bienvenue sur Sisi Daniel IA !</h1>
+    </main>
+  );
+}
+
+
